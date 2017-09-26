b28c3aa83538833789c9e1c7801212063c32b4a9
Jennie <jennie@jointrine.com>
2017-09-26 11:48:04 +0200

    Counter

--- src/game.js
+++ src/game.js
@@ -1,18 +1,14 @@
 import React from "react"
 import Card from "./card"
+import Counter from "./counter"
 import "./game.css";
 
 const Game = () => (
     <div>
         <div className="gameIntro">
             <h1>Crazy cat lady</h1>
-            <p className="lead">
-            Duis mollis, est non commodo luctus, nisi erat porttitor
-            ligula, eget lacinia odio sem nec elit. Vivamus sagittis
-            lacus vel augue laoreet rutrum faucibus dolor auctor. Sed
-            posuere consectetur est at lobortis.
-            </p>
         </div>
+        <Counter />
         <div className="game">
             <Card src="/images/kitten1.jpg" />
             <Card src="/images/kitten2.jpg" />
