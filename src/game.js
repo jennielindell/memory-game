0f73c48dfc7a8355c390120dfa5bb57ce9d3c1bc
Jennie <jennie@jointrine.com>
2017-09-26 14:51:37 +0200

    From const to class

--- src/game.js
+++ src/game.js
@@ -3,19 +3,31 @@ import Card from "./card"
 import Counter from "./counter"
 import "./game.css";
 
-const Game = () => (
-    <div>
-        <div className="gameIntro">
-            <h1>Crazy cat lady</h1>
-        </div>
-        <Counter />
-        <div className="game">
-            <Card src="/images/kitten1.jpg" />
-            <Card src="/images/kitten2.jpg" />
-            <Card src="/images/kitten3.jpg" />
-            <Card src="/images/kitten4.jpg" />
-        </div>
-    </div>
-)
+class Game extends React.Component {
+
+    state = {
+        cards: [
+            { src: "/images/kitten1.jpg" },
+            { src: "/images/kitten2.jpg" },
+            { src: "/images/kitten3.jpg" },
+            { src: "/images/kitten4.jpg" }
+        ]
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="gameIntro">
+                    <h1>Crazy cat lady</h1>
+                </div>
+                <div className="game">
+                    {this.state.cards.map((card) => (
+                        <Card src={card.src} />
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Game
