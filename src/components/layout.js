93f71db48e0d98034601e481148d61e242937df4
Jennie <jennie@jointrine.com>
2017-09-27 17:19:30 +0200

    Responsive

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,15 +2,17 @@ import React from "react";
 
 import Game from "./game";
 import Title from "./title";
+import "./layout.css"
 
 class Layout extends React.Component {
     render() {
         return (
-            <div>
+            <div className="layout">
                 <Title />
                 <Game />
             </div>
         )
     }
 }
+
 export default Layout
--- src/components/title.js
+++ src/components/title.js
@@ -3,7 +3,10 @@ import React from "react";
 class Title extends React.Component {
     render() {
         return (
-            <h1>Crazy cat lady</h1>
+            <div>
+                <h1>C.O.M.G</h1>
+                <p>(cuteness overload memory game)</p>
+            </div>
         )
     }
 }
